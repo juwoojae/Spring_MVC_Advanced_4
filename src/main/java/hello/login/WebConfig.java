ee26c692f8b2827fb0e970112c46efda0c8f4405
Juwoojae <juwqq1234@naver.com>
2025-10-02 14:38:04 +0900

    스프링 인터셉터 - 인증체크

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -3,6 +3,7 @@ package hello.login;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckIntercepter;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -24,10 +25,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**")  //인터셉터를 적용할 URL 패턴을 지정한다.
                 .excludePathPatterns("/css/", "/*.ico", "/error");   //인터셉터에서 제외할 패턴을 지정한다.
 
+        registry.addInterceptor(new LoginCheckIntercepter()) //등록할 인터셉터 지정
+                .order(2)  //인터셉터의 호출 순서를 지정한다.
+                .addPathPatterns("/**")  //인터셉터를 적용할 URL 패턴을 지정한다. (모든 경로에 대해서
+                .excludePathPatterns("/", "members/add", "/login", "logout", "/css/**", "/*.ico", "/error");   //인터셉터에서 제외할 패턴을 지정한다.
+
     }
 
     //@Bean
-    public FilterRegistrationBean logFilter(){
+    public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter()); //등록할 필터 지정.
         filterRegistrationBean.setOrder(1); //필터 체인에서의 순서
@@ -35,8 +41,9 @@ public class WebConfig implements WebMvcConfigurer {
 
         return filterRegistrationBean;
     }
-    @Bean
-    public FilterRegistrationBean loginCheckFilter(){
+
+    //@Bean
+    public FilterRegistrationBean loginCheckFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginCheckFilter()); //등록할 필터 지정.
         filterRegistrationBean.setOrder(2); //필터 체인에서의 순서
